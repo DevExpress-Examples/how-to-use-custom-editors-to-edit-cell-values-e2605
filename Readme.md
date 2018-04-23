842bd5d91825690293ce183f66d8f73690b26290
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Use Custom Editors to Edit Cell Values
+
+
+<p>This example shows how to use custom editors (<strong>ProgressBar</strong> and <strong>Slider</strong>) to display and edit the 'Quantity' column's values. Custom editors are defined via templates. The column's <i>DisplayTemplate</i> property defines a template that displays column values. The <i>EditTemplate</i> property defines a template that represents an editor used to edit cell values.</p>
+
+<br/>
+
+
---
+++ VB/CustomCellEditors/MainPage.xaml.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Markup
+
+Namespace CustomCellEditors
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = New ProductList()
+		End Sub
+	End Class
+	Public Class IntToDoubleConverter
+		Inherits MarkupExtension
+		Implements IValueConverter
+
+		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+			Return Me
+		End Function
+
+		Private Function IValueConverter_Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+			Return Convert.ToDouble(value)
+		End Function
+
+		Private Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+			Return Convert.ToInt32(value)
+		End Function
+	End Class
+End Namespace
+
