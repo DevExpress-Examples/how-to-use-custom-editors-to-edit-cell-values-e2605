39a1ffe39d67f07baff42ac0bb3ba70cf9edc19a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomCellEditors/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Controls;
+using DevExpress.Xpf.Core.WPFCompatibility;
+using System.Windows.Data;
+
+namespace CustomCellEditors {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = new ProductList();
+        }
+    }
+    public class IntToDoubleConverter : MarkupExtension, IValueConverter {
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return Convert.ToDouble(value);
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return Convert.ToInt32(value);
+        }
+    }
+}
